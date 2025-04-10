2098acd6b7e4e350716502708c12e3061c80f00e
Andrey Alikin <a.alikin@psyalter.com>
2025-04-10 06:53:29 +0300

    feat: move browserrouter to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import ListPage from './pages/ListPage';
 import SinglePage from './pages/SinglePage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<ListPage />} />
-      <Route path="/:id" element={<SinglePage />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ListPage />} />
+        <Route path="/:id" element={<SinglePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,8 +9,6 @@ const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
   root.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   );
 }
